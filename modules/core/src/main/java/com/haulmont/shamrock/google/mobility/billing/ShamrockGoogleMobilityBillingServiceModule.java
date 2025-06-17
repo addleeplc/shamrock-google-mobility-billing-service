2e1949be47152cab925a68b6f1fa87b1737859b7
root <root@shamrock-dev-environment-service-v1-7cdf45697-2nd8n>
2025-06-17 11:02:03 +0100

    Initial commit

---
+++ modules/core/src/main/java/com/haulmont/shamrock/google/mobility/billing/ShamrockGoogleMobilityBillingServiceModule.java
@@ -0,0 +1,23 @@
+package com.haulmont.shamrock.google.mobility.billing;
+
+import com.haulmont.monaco.annotations.Module;
+import com.haulmont.monaco.container.ModuleLoader;
+
+@Module(
+        name = "shamrock-google-mobility-billing-service-module",
+        depends = {
+                "monaco-jetty",
+                "monaco-core",
+                "monaco-config",
+                "monaco-graylog-reporter",
+                "monaco-sentry-reporter",
+                "monaco-ds", "monaco-ds-postgresql", "monaco-ds-sybase", "monaco-mybatis", "monaco-redis", "monaco-scheduler", "monaco-sql2o", "monaco-unirest"
+        }
+)
+public class ShamrockGoogleMobilityBillingServiceModule extends ModuleLoader {
+
+    public ShamrockGoogleMobilityBillingServiceModule () {
+        super();
+        packages(ShamrockGoogleMobilityBillingServiceModule.class.getPackageName());
+    }
+}
